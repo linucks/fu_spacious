4abfc5b706ff7ee55501d69fd377c9bbc5e4ee74
jmht <linucks42@gmail.com>
2018-02-22 14:52:23 +0000

    Added restrited pages

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  * farmurban functions
 */
 
-$FF_STAFF_PAGES = array( '/the-staff-room/' );
+$FF_STAFF_PAGES = array( '/the-staff-room/', '/zoom-room/', '/team-leader-instructions/', '/resources/' );
 $FF_STUDENT_PAGES = array( '/forums/forum/future-food-challenge-2018/', '/shop/', '/12-week-content/' );
 $FF_PAGES = array_merge($FF_STAFF_PAGES, $FF_STUDENT_PAGES);
 $FF_STAFF_MENUS = array( 'The Staff Room', 'Teachers Forum' );
